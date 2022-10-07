474cfa59ee48af1f1e1ad3da5cf5343e301d0c4b
gianilch <giancilchechen@gmail.com>
2022-10-07 17:19:09 -0300

    teste

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -3,5 +3,9 @@ import { Connection, createConnection, getConnectionOptions } from "typeorm";
 export default async (host = "database"): Promise<Connection> => {
   const defaultOptions = await getConnectionOptions();
 
-  return createConnection(Object.assign(defaultOptions, { host }));
+  return createConnection(
+    Object.assign(defaultOptions, {
+      host,
+    })
+  );
 };
