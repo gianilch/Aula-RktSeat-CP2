369139360d44d2ba9a01cd51a7ee3a4803d15081
gianilch <giancilchechen@gmail.com>
2023-08-01 10:25:28 -0300

    feat: novas rotas

--- src/shared/infra/http/routes/rental.routes.ts
+++ src/shared/infra/http/routes/rental.routes.ts
@@ -1,11 +1,14 @@
 import { CreateRentalController } from "@modules/rentals/useCases/createRental/CreateRentalController";
 import { Router } from "express";
+import { DevolutionRentalController } from "../../../../modules/rentals/useCases/devolutionRental/DevolutionRentalController";
 import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 
 const rentalRoutes = Router();
 
 const createRentalController = new CreateRentalController();
+const devolutionRentalController = new DevolutionRentalController();
 
 rentalRoutes.post("/", ensureAuthenticated, createRentalController.handle);
+rentalRoutes.post("/", ensureAuthenticated, devolutionRentalController.handle);
 
 export { rentalRoutes };
